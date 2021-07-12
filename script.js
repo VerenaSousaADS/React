5fcfad60cb4a06e479f30a6ba0b4b2851d402b47
Verena Sousa <verena.ads19@hotmail.com>
2021-07-12 16:52:41 -0300

    Inserção de arquivos e execução da Aula 01 - Introdução ao React

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Aula 01 - Introdução ao React */
+
+/* Aparece o contexto de SPA - Single Page Applications (Aplicação de Página Única), que utiliza apenas uma página HTML modificada conforme a interação com o usuário ou eventos para um sistema inteiro. 
+
+É nesse contexto em que surge o React, hoje ele é a principal biblioteca de frontend para a produção de SPA. 
+
+O React também inaugura o conceito de programação reativa em frontend. Que basicamente é a reconstrução da página sempre que houver mudança nos dados. Iso é feito de uma forma muito inteligente pois ele é capaz de renderizar apenas as partes da página que são dependentes dos dados que foram modificados e não o todo, tendo por resultado um ótimo desempenho.
+
+Para isso, o React usa uma arquitetura de componentes. Grande parte da programação em React está em saber dividir bem uma página em componentes e fazê-los de forma reutilizável e que admita composição com outros componentes.
+*/
